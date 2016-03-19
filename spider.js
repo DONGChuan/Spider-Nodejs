d22e5b356c9b575b2a3b2400aab70f193be1d7fa
dchuan <chuan.dong@amadeus.com>
2016-03-20 00:09:34 +0100

    save data

--- spider.js
+++ spider.js
@@ -40,12 +40,45 @@ http.get(opt, function(res) {
             };
 
             movies.push(movie);
+            downloadImg('img/', movie.picUrl);
         });
 
-        console.log(movies);
+        saveData('data/data.json', movies);
     });
 }).on('error', function(err) {
     console.log(err);
 });
+
+function saveData(path, movies) {
+    fs.writeFile(path, JSON.stringify(movies, null, 4), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log('Data saved');
+    });
+}
+
+function downloadImg(imgDir, url) {
+    http.get(url, function(res) {
+        var data = '';
+
+        res.setEncoding('binary');
+
+        res.on('data', function(chunk) {
+            data += chunk;
+        });
+
+        res.on('end', function() {
+            fs.writeFile(imgDir + path.basename(url), data, 'binary', function (err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log('Image downloaded', path.basename(url));
+            })
+        });
+    }).on('error', function(err) {
+        console.log(err);
+    });
+}
 
 
